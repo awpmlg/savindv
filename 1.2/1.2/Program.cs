d1fb11f5b5fe5977a799b629f30527eeaaa901b9
savindvvv@list.ru <savindvvv@list.ru>
2021-02-19 11:55:15 +0300

    123

---
+++ 1.2/1.2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace _1._2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     //------------------------------(1)
+      DriveInfo[] drives = DriveInfo.GetDrives();
+
+      foreach (DriveInfo drive in drives)
+      {
+        Console.WriteLine($"Название: {drive.Name}");
+        Console.WriteLine($"Тип: {drive.DriveType}");
+        if (drive.IsReady)
+        {
+          Console.WriteLine($"Объем диска: {drive.TotalSize}");
+          Console.WriteLine($"Свободное пространство: {drive.TotalFreeSpace}");
+          Console.WriteLine($"Метка: {drive.VolumeLabel}");
+        }
+        Console.WriteLine();
+      }
+       //------------------------------(2)
+        string path = @"C:\SomeDir2";
+      DirectoryInfo dirInfo = new DirectoryInfo(path);
+      if (!dirInfo.Exists)
+      {
+        dirInfo.Create();
+      }
+      Console.WriteLine("Введите строку для записи в файл:");
+      string text = Console.ReadLine();
+
+      using (FileStream fstream = new FileStream($"{path}\\note.txt", FileMode.Create))
+      {
+        byte[] array = System.Text.Encoding.Default.GetBytes(text);
+        fstream.Write(array, 0, array.Length);
+        Console.WriteLine("Текст записан в файл");
+      }
+
+      using (FileStream fstream = File.OpenRead($"{path}\\note.txt"))
+      {
+        byte[] array = new byte[fstream.Length];
+        fstream.Read(array, 0, array.Length);
+        string textFromFile = System.Text.Encoding.Default.GetString(array);
+        Console.WriteLine($"Текст из файла: {textFromFile}");
+      }
+
+      string pathq = @"C:\SomeDir2\note.txt";
+      FileInfo fileInf = new FileInfo(pathq);
+      if (fileInf.Exists)
+      {
+        fileInf.Delete();
+      }
+
+      string dirName = @"C:\SomeDir2";
+      Directory.Delete(dirName, true);
+      //------------------------------(3)
+
+      //------------------------------(4)
+
+      //------------------------------(5)
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ savdv/savdv/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO.Compression;
+using System.IO;
+
+namespace HelloApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string path = @"C:\test";
+      DirectoryInfo dirInfo = new DirectoryInfo(path);
+      if (!dirInfo.Exists)
+      {
+        dirInfo.Create();
+      }
+
+      string text = Console.ReadLine();
+
+      using (FileStream fstream = new FileStream($"{path}\\text.txt", FileMode.Create))
+      {
+        byte[] array = System.Text.Encoding.Default.GetBytes(text);
+        fstream.Write(array, 0, array.Length);
+        Console.WriteLine("Текст записан в файл");
+      }
+      string path1 = @"C:\testz";
+      DirectoryInfo dirInfo1 = new DirectoryInfo(path1);
+      if (!dirInfo1.Exists)
+      {
+        dirInfo1.Create();
+      }
+      //string path2 = @"D:\Documents";
+      //using (FileStream fstream = new FileStream($"{path2}\\test.zip", FileMode.Create)) { }
+
+      string sourceFolder = "C://test/"; // исходная папка
+      string zipFile = "D://Documents//test.zip"; // сжатый файл
+      string targetFolder = "C://testz"; // папка, куда распаковывается файл
+
+      ZipFile.CreateFromDirectory(sourceFolder, zipFile);
+      Console.WriteLine($"Папка {sourceFolder} архивирована в файл {zipFile}");
+      ZipFile.ExtractToDirectory(zipFile, targetFolder);
+
+      Console.WriteLine($"Файл {zipFile} распакован в папку {targetFolder}");
+
+      using (FileStream fstream = File.OpenRead($"{path1}\\text.txt"))
+      {
+        byte[] array = new byte[fstream.Length];
+        fstream.Read(array, 0, array.Length);
+        string textFromFile = System.Text.Encoding.Default.GetString(array);
+        Console.WriteLine($"Текст из файла: {textFromFile}");
+      }
+
+      Console.WriteLine("Удалить?(y/n)(если не удалите второй раз работать не будет, пока не удалите папки test, testz и файл D:\\Documents\\test.zip ");
+      string check = Console.ReadLine();
+      if (check=="y")
+      {
+        string dirName1 = @"C:\test";
+        Directory.Delete(dirName1, true);
+
+        string dirName2 = @"C:\testz";
+        Directory.Delete(dirName2, true);
+
+        string path5 = @"D:\Documents\test.zip";
+        FileInfo fileInf = new FileInfo(path5);
+        if (fileInf.Exists)
+        {
+          fileInf.Delete();
+        }
+      }
+
+      Console.ReadLine();
+
+    }
+  }
+}
